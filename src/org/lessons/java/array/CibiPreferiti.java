002e61c881ce83ec327f3de676ff4911ee567799
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-11-09 17:46:48 +0100

    added bonus of snak 3

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -18,5 +18,6 @@ public class CibiPreferiti {
 		System.out.println("Numero piatti: " + dishes.length);
 		System.out.println("Piatto preferito: " + dishes[0]);
 		System.out.println("Piatto preferito ma non troppo: " + dishes[dishes.length-1]);
+		System.out.println("Mediana: " + dishes[dishes.length/2]);
 	}
 }
