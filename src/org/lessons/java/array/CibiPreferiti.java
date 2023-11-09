afb56fa8961322c18b4cec7e29a69240060d6767
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-11-09 17:22:36 +0100

    added snack 3 (no bonus)

---
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -0,0 +1,22 @@
+package org.lessons.java.array;
+
+public class CibiPreferiti {
+	public static void main(String[] args) {
+		String[] dishes = {
+	            "Lasagna",
+	            "Pizza Margherita",
+	            "Sushi",
+	            "Bistecca alla griglia",
+	            "Insalata Caesar",
+	            "Pasta Carbonara",
+	            "Tacos al pastor",
+	            "Gelato alla vaniglia",
+	            "Cioccolato fondente",
+	            "Risotto ai frutti di mare"
+	        };
+		
+		System.out.println("Numero piatti: " + dishes.length);
+		System.out.println("Piatto preferito: " + dishes[0]);
+		System.out.println("Piatto preferito ma non troppo: " + dishes[dishes.length-1]);
+	}
+}
